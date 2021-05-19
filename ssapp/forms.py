ebf847a0c96df10224cc1b127002e01e65fc6bf5
chanrose <bdchanbroset@gmail.com>
2021-05-19 09:56:18 +0700

    update index

--- ssapp/forms.py
+++ ssapp/forms.py
@@ -1,4 +1,4 @@
-from .models import Class, Account, Curriculum, Course
+from .models import Account, Curriculum, Course
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
